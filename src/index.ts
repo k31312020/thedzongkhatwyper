3e1e73ed5b91cbd348452e83ed29832cd88c15be
Kuenzang <kuenzngg@gmail.com>
2021-09-15 16:12:27 +0600

    add supported devices

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,16 @@ import GameLoop from './game-loop';
 import './styles.css';
 
 // Entry point for the game
+
+// Check device
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    const overlay = document.createElement('div');
+    const overlayText = document.createElement('div');
+    overlay.classList.add('overlay');
+    overlayText.innerText = 'Device is not supported!';
+    overlayText.classList.add('overlay-text');
+    overlay.appendChild(overlayText);
+    document.body.appendChild(overlay);
+}
+
 new GameLoop().gameLoop();
